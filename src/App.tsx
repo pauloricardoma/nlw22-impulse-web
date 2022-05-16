ed88ce086d174d5b2254a4fd77284063260f21be
pauloricardoma <pauloricardo.m.almeida@gmail.com>
2022-05-16 13:11:32 -0300

    deploy nlw22 web

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Widget } from "./components/Widget";
+
+export function App() {
+  return (
+    <Widget />
+  )
+}
+
+// PONTOS DO TAILWIND
+// interface ButtonProps {
+//   text?: string;
+// }
+// function Button({text}: ButtonProps) {
+//   return <button className="bg-[#8B5CF6] px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">{text ?? 'Default'}</button>
+// }
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          300: '#996DFF',
+          500: '#8257e6',
+        }
+      },
+      borderRadius: {
+        md: '4px',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar'),
+  ],
+}
